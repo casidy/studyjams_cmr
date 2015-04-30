39e69efad524245a1496246088f297335372131b
casidy <lockevin@ymail.com>
2015-04-30 06:49:27 +0200

    application pour un service vocal

---
+++ app/src/main/java/com/example/loic/smsspeech/Appelant.java
@@ -0,0 +1,20 @@
+package com.example.loic.smsspeech;
+
+public class Appelant {
+
+	private String oNom;
+	private String oNum;
+	
+	public String getoNom() {
+		return oNom;
+	}
+	public void setoNom(String oNom) {
+		this.oNom = oNom;
+	}
+	public String getoNum() {
+		return oNum;
+	}
+	public void setoNum(String oNum) {
+		this.oNum = oNum;
+	}
+}
